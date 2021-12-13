1ea8151b46ca1ce248683df5e5702ad872f5d5a0
Yehya <40221838+BalderasBa@users.noreply.github.com>
2021-12-13 16:04:13 +0100

    Gallery + Timeline html, css and script. udating images too

--- script.js
+++ script.js
@@ -33,3 +33,56 @@ window.onscroll = () => {
   }
 };
 // Our Skills Section --------------------- ---------------------
+// --------------------- Our Gallery Section ---------------------
+const galleryImages = document.querySelectorAll(".images-box img");
+galleryImages.forEach((img) => {
+  img.addEventListener("click", (e) => {
+    // create overllay & addi it to body
+    let overllay = document.createElement("div");
+    overllay.className = "popup-overllay";
+    document.body.appendChild(overllay);
+    // create popup box
+    let popupBox = document.createElement("div");
+    popupBox.className = "popup-box";
+
+    // put alt text in head of img
+    if (img.alt !== null) {
+      let imgHead = document.createElement("h3");
+      let imgtext = document.createTextNode(img.alt);
+
+      imgHead.appendChild(imgtext);
+      popupBox.appendChild(imgHead);
+    }
+
+    // create image
+    let popupImage = document.createElement("img");
+    popupImage.src = img.src;
+
+    // put img in popup & popup in body
+    popupBox.appendChild(popupImage);
+    document.body.appendChild(popupBox);
+
+    //create close button
+    const closeBtn = document.createElement("span");
+    let closeBtnText = document.createTextNode("X");
+    closeBtn.appendChild(closeBtnText);
+    closeBtn.className = "close-button";
+    popupBox.appendChild(closeBtn);
+
+    //close popup with click on overlay
+    overllay.addEventListener("click", function (e) {
+      document.querySelector(".popup-overllay").remove();
+      popupBox.remove();
+    });
+  });
+});
+// close button
+document.addEventListener("click", (e) => {
+  // if popup opned remove it with overlay
+  if (e.target.className == "close-button") {
+    e.target.parentNode.remove();
+    document.querySelector(".popup-overllay").remove();
+  }
+});
+
+// Our Gallery Section --------------------- ---------------------
